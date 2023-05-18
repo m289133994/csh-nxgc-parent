3e0c5c02e55cddc272cf191fa3542e05f2da5584
csh <839196031@qq.com>
2023-05-18 18:58:15 +0800

    Initial commit

---
+++ src/main/java/com/csh/CshNxgcParentApplication.java
@@ -0,0 +1,13 @@
+package com.csh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CshNxgcParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CshNxgcParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csh/CshNxgcParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CshNxgcParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
